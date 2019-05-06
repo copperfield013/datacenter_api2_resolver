687c4268d56fb40882c280ab57889eee6e193638
Copperfield <copperfield013@gmail.com>
2019-05-06 14:20:33 +0800

    移除es5化

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-export {default as HelloWorld} from './lib/HelloWorld.js';
-export {default as hello} from './lib/hello.js';
-export {default as Utils} from './lib/Utils.js';
+export {default as HelloWorld} from './src/HelloWorld.js';
+export {default as hello} from './src/hello.js';
+export {default as Utils} from './src/Utils.js';
+export {default as DateFormat} from './src/DateFormat.js';
---
+++ src/DateFormat.js
@@ -0,0 +1,6 @@
+export default {
+	formatDate	: function(date, fmt){
+		console.log(date, fmt);
+		console.log('5444111');
+	}
+}
